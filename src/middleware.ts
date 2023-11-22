cedfb8695823e89c7b5bab408f6a4ead2ce166cb
Janna Gertz <janna.lynn@web.de>
2023-11-22 18:29:49 +0100

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ["/", "/index", "/*"],
+  matcher: ["/", "/index", "/moni"],
 };
 
 export function middleware(req: NextRequest) {
